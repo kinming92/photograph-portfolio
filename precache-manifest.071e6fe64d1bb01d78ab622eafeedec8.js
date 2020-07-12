3a8fdd7f43a7f4a13e0ddf92512b7ab5d46f69b8
kinming92 <kinming.klm@gmail.com>
2020-07-11 18:56:17 -0700

    Updates

---
+++ precache-manifest.071e6fe64d1bb01d78ab622eafeedec8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99dacb1ac3bbe6828593519c5384c101",
+    "url": "/photograph-portfolio/index.html"
+  },
+  {
+    "revision": "b71abb7b8e5a3be32701",
+    "url": "/photograph-portfolio/static/css/2.6886caac.chunk.css"
+  },
+  {
+    "revision": "dc17f91fb11b252bbd65",
+    "url": "/photograph-portfolio/static/css/main.0b0c542c.chunk.css"
+  },
+  {
+    "revision": "b71abb7b8e5a3be32701",
+    "url": "/photograph-portfolio/static/js/2.d1fde6e8.chunk.js"
+  },
+  {
+    "revision": "8390064e73dd8ad4676060139e575712",
+    "url": "/photograph-portfolio/static/js/2.d1fde6e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc17f91fb11b252bbd65",
+    "url": "/photograph-portfolio/static/js/main.0f139676.chunk.js"
+  },
+  {
+    "revision": "e3002bf4609ef06144f6",
+    "url": "/photograph-portfolio/static/js/runtime-main.2635c758.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/photograph-website/precache-manifest.5c2e837582b98e218374af6bb82d03b1.js"
+  "/photograph-portfolio/precache-manifest.071e6fe64d1bb01d78ab622eafeedec8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/photograph-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/photograph-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
